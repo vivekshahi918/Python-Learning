fd37a3362b7aade3c9a597b20a05fb77a10fda86
Vivek Shahi <shahivivek503@gmail.com>
2026-06-29 13:19:18 +0530

    Day 01 of Week 01

---
+++ W-1/d1.py
@@ -0,0 +1,16 @@
+# age = 25
+# print(str(age))
+
+# tax = 12.5 / 100
+# price = 100.50
+# price = price * tax
+
+# print(price)
+# ans = "Hello, World!, Myself Vivek Shahi \n and this is n\'t java code "
+# print(ans)
+
+# List
+List = [1, 2, 3, 4, 5]
+print(List)
+List.append(7**3)
+print(List)
