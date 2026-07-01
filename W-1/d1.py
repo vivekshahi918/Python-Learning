5f8db993475631ab8639d9286a753828c89de4ae
Vivek Shahi <shahivivek503@gmail.com>
2026-07-01 10:32:45 +0530

    Day1 Completed Basic of Python

--- W-1/d1.py
+++ W-1/d1.py
@@ -9,8 +9,95 @@
 # ans = "Hello, World!, Myself Vivek Shahi \n and this is n\'t java code "
 # print(ans)
 
-# List
-List = [1, 2, 3, 4, 5]
-print(List)
-List.append(7**3)
-print(List)
+# # List
+# List = [1, 2, 3, 4, 5]
+# print(List)
+# List.append(7**3)
+# print(List)
+
+# In string this fucntion is predined to return the length of the string
+# name = "Vivek Shahi"
+# print(name.capitalize())
+# print(name.casefold())
+# print(name.encode())
+# print(name.find("Shahi"))
+# print(name.expandtabs())
+# print(len(name))
+# print(name.isalnum())
+# print(name.isalpha())
+# print(name.isascii())
+# print(name.isdecimal())
+# print(name.isdigit())
+# print(name.islower())
+# print(name.isupper())
+# print(name.strip())
+# print(name.swapcase())
+# print(name.title())
+
+
+# name = "Vivek Shahi"
+# age = "23"
+
+# print(f"My name is {name} and my age is {age}")
+
+# import datetime
+# today = datetime.date.today()
+# print(f"Today's date is: {today:%B %d, %Y}")
+
+# student1 = "Vivek"
+# student2 = "Anand"
+# student3 = "Rohit"
+
+# print(f"There are 3 students and their names are {student1}, {student2}, and {student3}.")
+
+# name = "Vivek"
+# name.upper()
+# print(name)
+# name = name.upper()
+# print(name)
+
+# Common conersion type
+# age = "18"
+# print(int(age) + 2)
+# print(float(age) + 2)
+# print(str(age) + " is my age")
+# print(bool(age))
+# print(list(age))
+# print(tuple(age))
+# print(set(age))
+
+# print("5"+ str(5))
+# a =[1]
+# b = [1]
+# c = a
+
+# print(id(a))
+# print(id(b))
+# # c value is same as a
+# print(id(c)) 
+
+# 1
+s = "python"
+t = s.replace("p", "j")
+print(s)
+print(t)
+
+# 2
+print(5 + 3.0)
+
+# 3
+print("10" + "20")
+
+# 4
+print(int("10") + 20)
+
+# 5
+a = [1, 2]
+b = [1, 2]
+print(a == b)
+print(a is b)
+
+# 6
+x = None
+print(x is None)
+print(x == None)
